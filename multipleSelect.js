838100bf6bb1f7b3b2283d67bcea7864c069c7c2
hiyshamsts <36024963+hiyshamsts@users.noreply.github.com>
2018-05-06 23:00:34 +0800

    standalone proptypes

--- multipleSelect.js
+++ multipleSelect.js
@@ -4,7 +4,8 @@
  * www.atasmohammadi.net
  * version 1.0
  */
-import React, {Component, PropTypes} from "react";
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 import {
   Text,
   View,
